6ca7d02b3456845dac99fced1046a3a546ef62c6
Prabaswara24 <ragnabase69@gmail.com>
2022-06-18 20:34:25 +0700

    ta gan

---
+++ app/src/main/java/com/example/tugasakhirgan/chat.java
@@ -0,0 +1,27 @@
+package com.example.tugasakhirgan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void openWA(View view) {
+        String number = "6282230535038";
+        String url = "https://api.whatsapp.com/send?phone="+number;
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setPackage("com.whatsapp");
+        i.setData(Uri.parse(url));
+        startActivity(i);
+    }
+}
