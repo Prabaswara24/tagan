c707e268a9dc11eddec3a94b19eb811f8a630ce5
sendy452 <sendyivenyulian452@gmail.com>
2022-06-29 21:18:45 +0700

    update register and nohp

--- app/src/main/java/com/example/tugasakhirgan/Register.java
+++ app/src/main/java/com/example/tugasakhirgan/Register.java
@@ -1,37 +1,55 @@
 package com.example.tugasakhirgan;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 public class Register extends AppCompatActivity {
 
     private static final String TAG = "RegisterActivity";
     private static final String URL_FOR_REGISTRATION = "https://webadminbensae.my.id/api_ta/UserController/registrasi";
+
     ProgressDialog progressDialog;
 
-    private EditText signupInputName, signupInputEmail, signupInputPassword, signupInputAge;
+    private EditText signupInputName, signupInputEmail, signupInputPassword, signupInputAge, signupInputNo;
     private Button btnSignUp;
     private Button btnLinkLogin;
+    ImageView image;
     private RadioGroup genderRadioGroup;
 
     @Override
@@ -47,6 +65,17 @@ public class Register extends AppCompatActivity {
         signupInputEmail = findViewById(R.id.signup_input_email);
         signupInputPassword = findViewById(R.id.signup_input_password);
         signupInputAge = findViewById(R.id.signup_input_age);
+        signupInputNo = findViewById(R.id.signup_input_no);
+        image = (ImageView)findViewById(R.id.image);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.parse("package:" + getPackageName()));
+            finish();
+            startActivity(intent);
+            return;
+        }
 
         btnSignUp = findViewById(R.id.btn_signup);
         btnLinkLogin = findViewById(R.id.btn_link_login);
@@ -55,7 +84,35 @@ public class Register extends AppCompatActivity {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                submitForm();
+                if (signupInputName.getText().toString().trim().isEmpty()) {
+                    signupInputName.setError("Massukkan Nama");
+                    signupInputName.requestFocus();
+                    return;
+                }
+                if (signupInputEmail.getText().toString().trim().isEmpty()) {
+                    signupInputEmail.setError("Massukkan Email");
+                    signupInputEmail.requestFocus();
+                    return;
+                }
+                if (signupInputPassword.getText().toString().trim().isEmpty()) {
+                    signupInputPassword.setError("Massukkan Password");
+                    signupInputPassword.requestFocus();
+                    return;
+                }
+                if (signupInputAge.getText().toString().trim().isEmpty()) {
+                    signupInputAge.setError("Massukkan Alamat");
+                    signupInputAge.requestFocus();
+                    return;
+                }
+                if (signupInputNo.getText().toString().trim().isEmpty()) {
+                    signupInputNo.setError("Massukkan No.HP");
+                    signupInputNo.requestFocus();
+                    return;
+                }
+
+                //if everything is ok we will open image chooser
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, 100);
             }
         });
         btnLinkLogin.setOnClickListener(new View.OnClickListener() {
@@ -68,8 +125,37 @@ public class Register extends AppCompatActivity {
         });
     }
 
-    private void submitForm() {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
 
+            //getting the image Uri
+            Uri imageUri = data.getData();
+            try {
+                //getting bitmap object from uri
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+
+                //displaying selected image to imageview
+                image.setImageBitmap(bitmap);
+
+                //calling the method uploadBitmap to upload image
+                uploadBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    private void uploadBitmap(final Bitmap bitmap) {
+
+        //getting the tag from the edittext
         int selectedId = genderRadioGroup.getCheckedRadioButtonId();
         String gender;
         if (selectedId == R.id.female_radio_btn)
@@ -77,82 +163,168 @@ public class Register extends AppCompatActivity {
         else
             gender = "Male";
 
-        registerUser(signupInputName.getText().toString(),
-                signupInputEmail.getText().toString(),
-                signupInputPassword.getText().toString(),
-                gender,
-                signupInputAge.getText().toString());
-    }
-
-    private void registerUser(final String name, final String email, final String password,
-                              final String gender, final String dob) {
-        // Tag used to cancel the request
-        String cancel_req_tag = "register";
+        String nama = signupInputName.getText().toString();
+        String email = signupInputEmail.getText().toString();
+        String pass = signupInputPassword.getText().toString();
+        String alamat = signupInputAge.getText().toString();
+        String no = signupInputNo.getText().toString();
 
         progressDialog.setMessage("Adding you ...");
         showDialog();
 
-        StringRequest strReq = new StringRequest(Request.Method.POST,
-                URL_FOR_REGISTRATION, new Response.Listener<String>() {
+        //our custom volley request
+        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, URL_FOR_REGISTRATION,
+                new Response.Listener<NetworkResponse>() {
 
-            @Override
-            public void onResponse(String response) {
-                Log.d(TAG, "Register Response: " + response);
-                hideDialog();
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        System.out.println(response);
+                        try {
+                            JSONObject obj = new JSONObject(new String(response.data));
+                            System.out.println(response.data);
+                            String user = obj.getJSONObject("data").getString("name");
+                            Toast.makeText(getApplicationContext(), "Hi " + user + ", You are successfully Added!", Toast.LENGTH_SHORT).show();
 
-                try {
-                    JSONObject jObj = new JSONObject(response);
-                    boolean error = jObj.getBoolean("status");
-
-                    if (!error) {
-                        String user = jObj.getJSONObject("user").getString("name");
-                        Toast.makeText(getApplicationContext(), "Hi " + user + ", You are successfully Added!", Toast.LENGTH_SHORT).show();
-
-                        // Launch login activity
-                        Intent intent = new Intent(
-                                Register.this,
-                                LoginActivity.class);
+                            // Launch login activity
+                            Intent intent = new Intent(Register.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        hideDialog();
+                        Log.e(TAG, "Registration Error: " + error.getMessage());
+                        Toast.makeText(getApplicationContext(), "You are successfully Register!", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(Register.this, LoginActivity.class);
                         startActivity(intent);
                         finish();
-                    } else {
-
-                        String errorMsg = jObj.getString("error_msg");
-                        Toast.makeText(getApplicationContext(),
-                                errorMsg, Toast.LENGTH_LONG).show();
                     }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                }) {
 
-            }
-        }, new Response.ErrorListener() {
-
+            /*
+             * If you want to add more parameters with the image
+             * you can do it here
+             * here we have only one parameter with the image
+             * which is tags
+             * */
             @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, "Registration Error: " + error.getMessage());
-                Toast.makeText(getApplicationContext(),
-                        error.getMessage(), Toast.LENGTH_LONG).show();
-                hideDialog();
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() {
-                // Posting params to register url
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("nama", name);
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("nama", nama);
                 params.put("email", email);
-                params.put("no", "0");
-                params.put("password", password);
+                params.put("no", no);
+                params.put("password", pass);
                 params.put("gender", gender);
-                params.put("address", dob);
-                params.put("foto_ktp", "");
+                params.put("address", alamat);
+                return params;
+            }
+
+            /*
+             * Here we are passing image by renaming it with a unique name
+             * */
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                long imagename = System.currentTimeMillis();
+                params.put("foto_ktp", new DataPart(imagename + ".png", getFileDataFromDrawable(bitmap)));
                 return params;
             }
         };
-        // Adding request to request queue
-        VolleySingleton.getInstance(this).addToRequestQueue(strReq);
-    }
 
+        //adding the request to volley
+        Volley.newRequestQueue(this).add(volleyMultipartRequest);
+    }
+//    private void submitForm() {
+//
+//        int selectedId = genderRadioGroup.getCheckedRadioButtonId();
+//        String gender;
+//        if (selectedId == R.id.female_radio_btn)
+//            gender = "Female";
+//        else
+//            gender = "Male";
+//
+//        registerUser(signupInputName.getText().toString(),
+//                signupInputEmail.getText().toString(),
+//                signupInputPassword.getText().toString(),
+//                gender,
+//                signupInputAge.getText().toString(),
+//                signupInputNo.getText().toString());
+//    }
+//
+//    private void registerUser(final String name, final String email, final String password,
+//                              final String gender, final String dob, final String no) {
+//        // Tag used to cancel the request
+//        String cancel_req_tag = "register";
+//
+//        progressDialog.setMessage("Adding you ...");
+//        showDialog();
+//
+//        StringRequest strReq = new StringRequest(Request.Method.POST,
+//                URL_FOR_REGISTRATION, new Response.Listener<String>() {
+//
+//            @Override
+//            public void onResponse(String response) {
+//                Log.d(TAG, "Register Response: " + response);
+//                hideDialog();
+//
+//                try {
+//                    JSONObject jObj = new JSONObject(response);
+//                    boolean error = jObj.getBoolean("status");
+//
+//                    if (!error) {
+//                        String user = jObj.getJSONObject("user").getString("name");
+//                        Toast.makeText(getApplicationContext(), "Hi " + user + ", You are successfully Added!", Toast.LENGTH_SHORT).show();
+//
+//                        // Launch login activity
+//                        Intent intent = new Intent(
+//                                Register.this,
+//                                LoginActivity.class);
+//                        startActivity(intent);
+//                        finish();
+//                    } else {
+//
+//                        String errorMsg = jObj.getString("error_msg");
+//                        Toast.makeText(getApplicationContext(),
+//                                errorMsg, Toast.LENGTH_LONG).show();
+//                    }
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        }, new Response.ErrorListener() {
+//
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Log.e(TAG, "Registration Error: " + error.getMessage());
+//                Toast.makeText(getApplicationContext(),
+//                        error.getMessage(), Toast.LENGTH_LONG).show();
+//                hideDialog();
+//            }
+//        }) {
+//            @Override
+//            protected Map<String, String> getParams() {
+//                // Posting params to register url
+//                Map<String, String> params = new HashMap<String, String>();
+//                params.put("nama", name);
+//                params.put("email", email);
+//                params.put("no", no);
+//                params.put("password", password);
+//                params.put("gender", gender);
+//                params.put("address", dob);
+//                params.put("foto_ktp", "");
+//                return params;
+//            }
+//        };
+//        // Adding request to request queue
+//        VolleySingleton.getInstance(this).addToRequestQueue(strReq);
+//    }
+//
     private void showDialog() {
         if (!progressDialog.isShowing())
             progressDialog.show();
